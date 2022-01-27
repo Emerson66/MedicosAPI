81357f16bde8b5662f102c00636780bf4b689cd3
Emerson66 <emersonreisdacosta@gmail.com>
2022-01-26 20:16:08 -0400

    Projeto Criado

---
+++ src/main/java/br/com/buritiscript/MedicosAPI/MedicosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.MedicosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicosApiApplication.class, args);
+	}
+
+}
